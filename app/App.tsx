57320cc691d113e8c7d602bf58373649e6bda727
anabmartins <ana_beatriz-martins@estudante.sesisenai.org.br>
2023-08-07 15:42:29 -0300

    theme andd components

--- app/App.tsx
+++ app/App.tsx
@@ -1,29 +1,11 @@
 import * as React from 'react';
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Image } from 'react-native';
 import { Routes } from './src/routes'
-
+import { SignIn } from './src/screens/SingIn';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      {/* <Text>CantinaGo</Text>
-      <StatusBar style="auto" />
-      <Image style={styles.logo} source={require('./assets/img/coxinha.png')}/> */}
       <Routes/>
-    </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
-  route:{
-    width: "100%",
-  },
-  logo:{
-    width: 200,
-    height: 200,
-  }
-});
+
---
+++ app/src/components/Card.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react';
+import { View } from 'react-native';
+import { Heading, HStack, Image, Text, VStack, Icon } from 'native-base';
+
+export function Card() {
+  return (
+    <View>
+
+    </View>
+  );
+}
