8b7d65734481ad84a7711118c26ffb82807975af
ANA BEATRIZ MARTINS <ana_beatriz-martins@estudante.sesisenai.org.br>
2023-08-07 09:01:54 -0300

    mudanças na tela

--- app/App.tsx
+++ app/App.tsx
@@ -1,5 +1,5 @@
+import * as React from 'react';
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
 import { StyleSheet, Text, View, Image } from 'react-native';
 import {TabRoutes} from './src/routes/app.route'
 
