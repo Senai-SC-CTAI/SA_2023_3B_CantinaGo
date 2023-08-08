cabec9d9f8a35723229b7053eb84d2bb943f2ade
ANA BEATRIZ MARTINS <ana_beatriz-martins@estudante.sesisenai.org.br>
2023-08-08 14:24:32 -0300

    att home

--- app/src/screens/Home/index.tsx
+++ app/src/screens/Home/index.tsx
@@ -4,14 +4,28 @@ import { View, StyleSheet, Text } from 'react-native';
 export function Home() {
   return (
     <View style={styles.container}>
-      <Text>Home</Text>
+      <Text style={styles.text}>Bom dia!</Text>
+      <View style={styles.line}></View>
     </View>
-);
+  );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: 'white'
+    backgroundColor: 'white',
   },
-})
+  text: {
+    marginTop: 40,
+    marginLeft: 37,
+    fontSize: 39,
+    fontWeight: 'bold',
+  },
+  line: {
+    width: 153,
+    height: 3.5,
+    backgroundColor: '#FA321A',
+    marginTop: 9,
+    marginLeft: 37,
+  },
+});
