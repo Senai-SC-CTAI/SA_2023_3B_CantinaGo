8341a287b6fd4a49d0746e17f3c23186d85b1324
MARIA EDUARDA GARIBALDI DA CRUZ <maria_eg_cruz@estudante.sesisenai.org.br>
2023-08-09 12:49:17 -0300

    Tela de Login

--- app/App.tsx
+++ app/App.tsx
@@ -1,10 +1,10 @@
 import * as React from 'react';
 import { Routes } from './src/routes'
-import { SignIn } from './src/screens/SingIn';
+import SignIn  from './src/screens/SingIn';
 
 export default function App() {
   return (
-      <Routes/>
+      <SignIn/>
   );
 }
 
--- app/src/screens/SingIn/index.tsx
+++ app/src/screens/SingIn/index.tsx
@@ -1,16 +1,74 @@
-import * as React from 'react';
-import { View, StyleSheet } from 'react-native';
+import React from 'react';
+import { View, Text, TextInput, TouchableOpacity, StyleSheet } from 'react-native';
 
-export function SignIn() {
+const App = () => {
   return (
     <View style={styles.container}>
-
+      <Text style={styles.title}>Login</Text>
+      <TextInput
+        style={styles.input}
+        placeholder="seuemail@estudante.sesisenai.org.br"
+      />
+      <TextInput
+        style={styles.input}
+        placeholder="Senha"
+        secureTextEntry
+      />
+      <TouchableOpacity style={styles.button}>
+        <Text style={styles.buttonText}>Entrar</Text>
+      </TouchableOpacity>
+      <Text style={styles.texty}>Não tem login?</Text>
+      <Text style={styles.cadastro}>Faça cadastro</Text>
     </View>
-);
-}
+  );
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    display:'flex',
+    backgroundColor: '#f5f5f5',
   },
-})
+  title: {
+    color: '#FA321A',
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginBottom: 20,
+    fontFamily:'inter',
+  },
+  texty: {
+    fontSize: 15,
+    marginBottom: 10,
+  },
+  cadastro: {
+    fontSize: 15,
+    marginBottom: 10,
+    color: '#FA321A',
+  },
+  input: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    display:'flex',
+    width: '80%',
+    height: 40,
+    borderColor: '#F4F4F4',
+    borderWidth: 1,
+    borderRadius: 5,
+    paddingHorizontal: 10, // Adicionado padding horizontal
+    marginBottom: 10,
+  },
+  button: {
+    backgroundColor: '#FA321A',
+    paddingHorizontal: 20,
+    paddingVertical: 10,
+    borderRadius: 5,
+  },
+  buttonText: {
+    color: 'white',
+    fontWeight: 'bold',
+  },
+});
+
+export default App;
