49a279e4a893f41dd3eec7a950810032745f6baa
BERNARDO MACHADO <bernardo_machado1@estudante.sesisenai.org.br>
2023-08-09 12:57:30 -0300

    TelaUsers

--- app/src/screens/User/index.tsx
+++ app/src/screens/User/index.tsx
@@ -1,9 +1,24 @@
 import * as React from 'react';
-import { View, StyleSheet } from 'react-native';
+import { View, StyleSheet, Image, TextInput, Text } from 'react-native';
 
 export function User() {
   return (
     <View style={styles.container}>
+      <View style={styles.profileContent}>
+      <View style={styles.profilePicture}></View>
+      <View style={styles.profileInfo}>
+      </View>
+      </View>
+      <View style={styles.inputsInfo}>
+        <TextInput style={styles.inputs} placeholder="Email"></TextInput>
+        <TextInput style={styles.inputs} placeholder="Data de nascimento"></TextInput>
+        <TextInput style={styles.inputs} placeholder="Turma"></TextInput>
+        <TextInput style={styles.inputs} placeholder="Telefone"></TextInput>
+      </View>
+      <View style={styles.feedback}>
+        <Text>Algum comentário</Text>
+        <Text>Deixe-o aqui</Text>
+      </View>
 
     </View>
 );
@@ -13,4 +28,10 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
   },
+
+  profileContent:{
+
+  },
+  inputsInfo:{}
+  
 })
