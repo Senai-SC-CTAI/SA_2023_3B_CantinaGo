1c583aa22feead8513b4ee1a9faa16e11765022f
BeMachado23 <bemachado.floripa@gmail.com>
2023-08-09 22:16:31 -0300

    estilizando a tela de usuario

--- app/App.tsx
+++ app/App.tsx
@@ -1,11 +1,10 @@
 import * as React from 'react';
-import { Routes } from './src/routes'
+import { Routes } from './src/routes/bottomTabs';
 import { SignIn } from './src/screens/SingIn';
 
 export default function App() {
   return (
-      <SignIn/>
+      <Routes/>
   );
 }
-
 
--- app/src/screens/User/index.tsx
+++ app/src/screens/User/index.tsx
@@ -1,9 +1,25 @@
+import { background } from 'native-base/lib/typescript/theme/styled-system';
 import * as React from 'react';
-import { View, StyleSheet } from 'react-native';
+import { View, StyleSheet, Image, TextInput, Text } from 'react-native';
 
 export function User() {
   return (
     <View style={styles.container}>
+      <View style={styles.profileContent}>
+      <View style={styles.profilePicture}></View>
+      <View style={styles.profileInfo}>
+      </View>
+      </View>
+      <View style={styles.inputsInfo}>
+        <TextInput style={styles.inputs} placeholder="email" placeholderTextColor={"#6A6A6A"}></TextInput>
+        <TextInput style={styles.inputs} placeholder="data de nascimento" placeholderTextColor={"#6A6A6A"}></TextInput>
+        <TextInput style={styles.inputs} placeholder="turma" placeholderTextColor={"#6A6A6A"}></TextInput>
+        <TextInput style={styles.inputs} placeholder="telefone" placeholderTextColor={"#6A6A6A"}></TextInput>
+      </View>
+      <View style={styles.feedback}>
+        <Text style ={{color: "9E9D9D"}}>Algum comentário</Text>
+        <Text>Deixe-o aqui</Text>
+      </View>
 
     </View>
 );
@@ -12,5 +28,26 @@ export function User() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    justifyContent:'center',
+    display:'flex',
+    alignItems: 'center',
+     
   },
+
+  profileContent:{
+  justifyContent:'center',
+  display:'flex',
+  textAlign: 'center',
+
+  },
+  inputsInfo:{
+
+  },
+  inputs:{
+    width: 290,
+    height: 37,
+    backgroundColor: "#F4F4F4",
+    marginBottom: 20,
+    borderRadius: 10,
+  }
 })
