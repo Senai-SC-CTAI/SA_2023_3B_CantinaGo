84482617feffb155cae235fa49aeba879f0cdce3
BeMachado23 <bemachado.floripa@gmail.com>
2023-08-09 22:28:23 -0300

    Estilizando tela de Usuario

--- app/src/screens/User/index.tsx
+++ app/src/screens/User/index.tsx
@@ -1,3 +1,4 @@
+import { background } from 'native-base/lib/typescript/theme/styled-system';
 import * as React from 'react';
 import { View, StyleSheet, Image, TextInput, Text } from 'react-native';
 
@@ -10,13 +11,13 @@ export function User() {
       </View>
       </View>
       <View style={styles.inputsInfo}>
-        <TextInput style={styles.inputs} placeholder="Email"></TextInput>
-        <TextInput style={styles.inputs} placeholder="Data de nascimento"></TextInput>
-        <TextInput style={styles.inputs} placeholder="Turma"></TextInput>
-        <TextInput style={styles.inputs} placeholder="Telefone"></TextInput>
+        <TextInput style={styles.inputs} placeholder="email" placeholderTextColor={"#6A6A6A"}></TextInput>
+        <TextInput style={styles.inputs} placeholder="data de nascimento" placeholderTextColor={"#6A6A6A"}></TextInput>
+        <TextInput style={styles.inputs} placeholder="turma" placeholderTextColor={"#6A6A6A"}></TextInput>
+        <TextInput style={styles.inputs} placeholder="telefone" placeholderTextColor={"#6A6A6A"}></TextInput>
       </View>
       <View style={styles.feedback}>
-        <Text>Algum comentário</Text>
+        <Text style ={{color: "9E9D9D"}}>Algum comentário</Text>
         <Text>Deixe-o aqui</Text>
       </View>
 
@@ -27,11 +28,26 @@ export function User() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    justifyContent:'center',
+    display:'flex',
+    alignItems: 'center',
+     
   },
 
   profileContent:{
+  justifyContent:'center',
+  display:'flex',
+  textAlign: 'center',
 
   },
-  inputsInfo:{}
-  
+  inputsInfo:{
+
+  },
+  inputs:{
+    width: 290,
+    height: 37,
+    backgroundColor: "#F4F4F4",
+    marginBottom: 20,
+    borderRadius: 10,
+  }
 })
