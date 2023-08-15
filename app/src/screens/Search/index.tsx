8eb11c5c541e6999c0012d4fc4cf5d7bb77cc200
unknown <enzobatista24@gmail.com>
2023-08-14 22:57:33 -0300

    Criando a tela de busca

--- app/src/screens/Search/index.tsx
+++ app/src/screens/Search/index.tsx
@@ -1,12 +1,49 @@
 import * as React from 'react';
-import { View, StyleSheet } from 'react-native';
+import { View, StyleSheet, Image, TextInput, Text, TouchableOpacity } from 'react-native';
+import { MaterialIcons, MaterialCommunityIcons, Feather } from '@expo/vector-icons';
+import { FontAwesome } from '@expo/vector-icons'; 
+import { useNavigation } from '@react-navigation/native'
+
 
 export function Search() {
+
   return (
     <View style={styles.container}>
+      <View style={styles.header}>
+        <Feather
+          name="arrow-left"
+          size={38}
+          color="#FA321A"
+          style={{ marginLeft: 20 }}
+        />
+        <Text style={styles.title}>Buscar</Text>
+      </View>
+
+      <View style={styles.inputSearchHolder}>
+        <FontAwesome 
+          name="search" 
+          size={24} 
+          color="black" />
+        <TextInput placeholder="Digite o que deseja buscar"></TextInput>
+      </View>
+
+      <View style={styles.searchItens}>
+        <View style={styles.salgados}>
+          <Text style={styles.ItenSearchItens}>Salgados</Text>
+        </View>
+        <View style={styles.doces}>
+          <Text style={styles.ItenSearchItens}>Doces</Text>
+        </View>
+        <View style={styles.sucos}>
+          <Text style={styles.ItenSearchItens}>Sucos</Text>
+        </View>
+        <View style={styles.snacks}>
+          <Text style={styles.ItenSearchItens}>Snacks</Text>
+        </View>
+      </View>
 
     </View>
-);
+  );
 }
 
 const styles = StyleSheet.create({
@@ -14,4 +51,19 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#FFFFFF',
   },
+  header: {
+    display: 'flex',
+    flexDirection: 'row',
+    width: '100%',
+    height: 90,
+    position: 'absolute',
+    top: 0,
+    alignItems: 'center'
+  },
+  inputSearchHolder: {
+    display:'flex',
+    flexDirection:'row',
+    backgroundColor:'#F4F4F4',
+    borderRadius:200,
+  }
 })
