f716d72ca3aa7df7a2369eb4e288d50a160615d0
venous666 <maria_eg_cruz@estudante.sesisenai.org.br>
2023-08-16 13:00:13 -0300

    new

--- app/src/screens/SignIn/index.tsx
+++ app/src/screens/SignIn/index.tsx
@@ -1,24 +1,10 @@
-import * as React from 'react';
+import React from 'react';
 import { View, Text, TextInput, TouchableOpacity, StyleSheet } from 'react-native';
-import { useNavigation } from '@react-navigation/native'
 
-import styles from './styles';
-
-export function SignIn() {
-
-  const navigation = useNavigation()
-
-  function SignIn(){
-    navigation.navigate('Route')
-  }
-  function SignUp(){
-    navigation.navigate('SignUp')
-  }
+export default function SingIn() {
   return (
     <View style={styles.container}>
-      
       <Text style={styles.title}>Login</Text>
-      
       <TextInput
         style={styles.input}
         placeholder="seuemail@estudante.sesisenai.org.br"
@@ -28,18 +14,60 @@ export function SignIn() {
         placeholder="Senha"
         secureTextEntry
       />
-      
-      <TouchableOpacity style={styles.button} onPress={(SignIn)}>
+      <TouchableOpacity style={styles.button}>
         <Text style={styles.buttonText}>Entrar</Text>
       </TouchableOpacity>
-      
       <Text style={styles.texty}>Não tem login?</Text>
-      
-      <TouchableOpacity style={styles.cadastro} onPress={(SignUp)}>
       <Text style={styles.cadastro}>Faça cadastro</Text>
-      </TouchableOpacity>
-
     </View>
   );
-}
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    display:'flex',
+    backgroundColor: '#f5f5f5',
+  },
+  title: {
+    color: '#FA321A',
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginBottom: 20,
+    fontFamily:'inter',
+  },
+  texty: {
+    fontSize: 15,
+    marginBottom: 10,
+  },
+  cadastro: {
+    fontSize: 15,
+    marginBottom: 10,
+    color: '#FA321A',
+  },
+  input: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    display:'flex',
+    width: '80%',
+    height: 40,
+    borderColor: '#F4F4F4',
+    borderWidth: 1,
+    borderRadius: 5,
+    paddingHorizontal: 10, // Adicionado padding horizontal
+    marginBottom: 10,
+  },
+  button: {
+    backgroundColor: '#FA321A',
+    paddingHorizontal: 20,
+    paddingVertical: 10,
+    borderRadius: 5,
+  },
+  buttonText: {
+    color: 'white',
+    fontWeight: 'bold',
+  },
+});
 
