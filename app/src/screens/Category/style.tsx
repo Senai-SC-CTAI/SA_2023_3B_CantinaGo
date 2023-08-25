0ccf10bebccb0940a7590908ebee57db79df626e
ENZO BATISTA DORNELES FRANCISCO <enzo_francisco@estudante.sesisenai.org.br>
2023-08-25 09:37:03 -0300

    Implementação de componentes na tela de categorias

--- app/src/screens/Category/style.tsx
+++ app/src/screens/Category/style.tsx
@@ -1,7 +1,28 @@
 import { StyleSheet } from "react-native"
 
 const styles = StyleSheet.create({
-
+    container: {
+      flex: 1,
+      display:'flex',
+      justifyContent: 'center',
+      alignItems:'center',
+      backgroundColor: '#FFFFFF'
+    },
+    header: {
+      display: 'flex',
+      flexDirection: 'row',
+      width: '100%',
+      height: 90,
+      top: 0,
+      alignItems: 'center',
+    },
+    title:{
+        fontSize:36,
+        color:"#000000",
+        marginLeft:20,
+        fontWeight:"bold",
+        marginRigth: 173,
+      },
    
   })
 
