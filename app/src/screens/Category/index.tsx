832033c5afddb87d1c56a29bfc5ed7670d450d9d
anabmartins <ana_beatriz-martins@estudante.sesisenai.org.br>
2023-08-28 12:20:54 -0300

    rota de category

--- app/src/screens/Category/index.tsx
+++ app/src/screens/Category/index.tsx
@@ -1,15 +1,23 @@
 import * as React from 'react';
 import { useState } from 'react';
+import { useNavigation } from '@react-navigation/native'; // Hook para navegação em uma aplicação React Navigation.
 import { View, Image, TextInput, Text, TouchableOpacity } from 'react-native';
 import { Categoria } from '../../components/Categoria';
 import styles from './style';
 import { Feather } from '@expo/vector-icons';
 
-export function Category() {
+export function Category() {  
+ 
+  const navigation = useNavigation();
+  
+  function back() {
+  navigation.navigate("Search");
+}
     return (
         <View style={styles.container}>
              <View style={styles.header}>
-        <TouchableOpacity>
+        <TouchableOpacity
+         onPress={back}>
           <Feather
             name="arrow-left"
             size={38}
