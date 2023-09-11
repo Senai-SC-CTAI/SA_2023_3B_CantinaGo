5dac88d54d050eecf7828f2039eae85aab613e4d
Ximbeh <senhorximbinha@gmail.com>
2023-09-10 23:32:16 -0300

    Teste 1 de designs para card, provavelmente meio porcoskkkk

--- app/src/screens/Food/styles.tsx
+++ app/src/screens/Food/styles.tsx
@@ -207,6 +207,123 @@ const styles = StyleSheet.create({
   // },
   
   //Design2
+  // container: {
+  //   flex: 1,
+  //   backgroundColor: '#FFFFFF',
+  //   display:'flex',
+  //   alignItems:'center',
+  // },
+
+  // header: {
+  //   display: 'flex',
+  //   flexDirection: 'row',
+  //   width: '100%',
+  //   height: 90,
+  //   top: 20,
+  // },
+
+  // title: {
+  //   fontSize:36,
+  //   color:"#000000",
+  //   marginLeft:20,
+  //   fontWeight:"bold"
+  // },
+
+  // card: {
+  //   backgroundColor: '#F4F4F4',
+  //   width: '60%',
+  //   paddingLeft: 20,
+  //   paddingTop: 25,
+  //   paddingBottom: 20,
+  //   borderRadius: 10,
+  //   marginBottom: 20,
+  //   resizeMode: 'contain',
+  //   position: 'relative'
+  // },
+
+  // backgroundImage: {
+  //   position: 'absolute',
+  //   top: 70,
+  //   left: 0,
+  //   width: '100%',
+  //   height: '100%',
+  //   zIndex: -1,
+  // },
+
+  // titleComida: {
+  //   fontSize:36,
+  //   color:"#000000",
+  //   fontFamily:"Inter_700Bold",
+  //   marginRight: 20
+  // },
+
+  // categoriaComida: {
+  //   fontSize:20,
+  //   color:"#7A7A7A",
+  //   fontFamily:"Inter_600SemiBold"
+  // },
+
+  // imagemComida: {
+  //   width: 230,
+  //   height: 230,
+  // },
+  
+  // calorias: {
+  //   fontSize:25,
+  //   color:"#FF4630",
+  //   marginBottom: 20,
+  //   fontFamily:"Inter_800ExtraBold",
+  //   textAlign: 'center',
+  //   marginRight: 20,
+  // },
+
+  // tituloIgredientes: {
+  //   color: '#5B5B5B',
+  //   fontWeight: 'bold'
+  // },
+
+  // igredientes: {
+  //   marginLeft: 5,
+  //   marginBottom: 20,
+  // },
+
+  // textoIgredientes: {
+  //   color: '#5B5B5B',
+  //   fontFamily:"Inter_400Regular",
+  //   fontSize:12,
+    
+  // },
+
+  // cardPrice: {
+  //   backgroundColor: '#F4F4F4',
+  //   width: '40%',
+  //   paddingTop: 10,
+  //   paddingBottom: 10,
+  //   borderRadius: 10,
+  //   marginBottom: 20,
+  //   marginTop: 50,
+  //   alignContent: 'center',
+  //   justifyContent: 'center',
+  //   alignItems: 'center',
+  //   resizeMode: 'contain',
+
+  //   shadowColor: "#000",
+  //   shadowOffset: {
+  //     width: 0,
+  //     height: 5,
+  //   },
+  //   shadowOpacity: 0.3,
+  //   shadowRadius: 5.00,
+  // },
+
+  // precoTexto: {
+  //   fontSize:20,
+  //   color:"#FA321A",
+  //   fontWeight:"bold",
+  //   textAlign: 'center',
+  // },
+
+  //Design 3
   container: {
     flex: 1,
     backgroundColor: '#FFFFFF',
@@ -230,9 +347,9 @@ const styles = StyleSheet.create({
   },
 
   card: {
-    backgroundColor: '#F4F4F4',
     width: '70%',
-    paddingLeft: 20,
+    paddingLeft: 40,
+    paddingRight: 50,
     paddingTop: 25,
     paddingBottom: 20,
     borderRadius: 10,
@@ -243,10 +360,10 @@ const styles = StyleSheet.create({
 
   backgroundImage: {
     position: 'absolute',
-    top: 70,
-    left: 0,
-    width: '100%',
-    height: '100%',
+    top: '-15vw',
+    left: '-33vw',
+    width: '135vw',
+    height: '100vh',
     zIndex: -1,
   },
 
@@ -265,20 +382,18 @@ const styles = StyleSheet.create({
   imagemComida: {
     width: 230,
     height: 230,
-    marginRight: 20,
+    
   },
   
   calorias: {
     fontSize:25,
-    color:"#FF4630",
+    color:"#FEFEFE",
     marginBottom: 20,
-    fontFamily:"Inter_600SemiBold",
-    textAlign: 'center'
+    fontFamily:"Inter_600SemiBold"
   },
 
   tituloIgredientes: {
-    color: '#5B5B5B',
-    fontWeight: 'bold'
+    color: '#FEFEFE'
   },
 
   igredientes: {
@@ -287,11 +402,12 @@ const styles = StyleSheet.create({
   },
 
   textoIgredientes: {
-    color: '#5B5B5B',
+    color: '#FEFEFE',
     fontFamily:"Inter_400Regular",
     fontSize:12,
     
   },
+
 
   cardPrice: {
     backgroundColor: '#F4F4F4',
@@ -300,7 +416,7 @@ const styles = StyleSheet.create({
     paddingBottom: 10,
     borderRadius: 10,
     marginBottom: 20,
-    marginTop: 50,
+    marginTop: 60,
     alignContent: 'center',
     justifyContent: 'center',
     alignItems: 'center',
@@ -320,9 +436,8 @@ const styles = StyleSheet.create({
     color:"#FA321A",
     fontWeight:"bold",
     textAlign: 'center',
-    marginRight: 20,
-  },
-  
+  }
 })
+
 
 export default styles; 
