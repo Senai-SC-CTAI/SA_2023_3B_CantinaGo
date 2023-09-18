2e75e3401e5b6f8e4e546e531959357e72e5c8ce
JAÍZA MONTEIRO DOS SANTOS FREIRE <jaiza_freire@estudante.sesisenai.org.br>
2023-09-18 12:09:14 -0300

    funcionario controller atualizado

--- back/src/main/java/com/example/cantinago/controller/FuncionarioController.java
+++ back/src/main/java/com/example/cantinago/controller/FuncionarioController.java
@@ -1,5 +1,49 @@
 package com.example.cantinago.controller;
+package com.example.cantinago.controller;
+import com.example.cantinago.model.Funcionario;
+import com.example.cantinago.repository.FuncionarioRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
+import org.springframework.http.ResponseEntity;
+@CrossOrigin(origins = "http://localhost:5173")//Endereço do front
+@RestController
+@RequestMapping("/funcionarios")
 
 public class FuncionarioController {
-    
+    @Autowired
+    private FuncionarioRepository funcionarioRepository;
+
+    @GetMapping
+    public List<Funcionario> listarFuncionarios() {
+    return funcionarioRepository.findAll();
+  }
+    @PostMapping
+    public Funcionario criarFuncionario(@RequestBody Funcionario funcionario) {
+    return funcionarioRepository.save(funcionario);
+  }
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> deletarFuncionario(@PathVariable Long id) {
+    if (funcionarioRepository.existsById(id)) {
+      funcionarioRepository.deleteById(id);
+      return ResponseEntity.ok("Funcionario deletado com sucesso");
+    } else {
+      return ResponseEntity.notFound().build();
+     }
+    }
+    @PutMapping("/{id}")
+    public ResponseEntity<Funcionario> atualizarFuncionario(@PathVariable Long id_funcionario, @RequestBody Funcionario funcionarioAtualizado) {
+        if (funcionarioRepository.existsById(id_funcionario)) {
+            Funcionario funcionario = funcionarioRepository.findById(id_funcionario).get();
+            funcionario.setNome(funcionarioAtualizado.getNome());
+            funcionario.setSobrenome(funcionarioAtualizado.getSobrenome());
+            funcionario.setEmail(funcionarioAtualizado.getEmail());
+
+            Funcionario funcionarioAtualizadoBD = funcionarioRepository.save(funcionario);
+            return ResponseEntity.ok(funcionarioAtualizadoBD);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
 }
+
