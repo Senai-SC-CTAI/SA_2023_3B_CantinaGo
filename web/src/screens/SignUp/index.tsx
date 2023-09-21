b4428ab0243ae97f28e38620281cd20d0cb75a2b
MARIA EDUARDA GARIBALDI DA CRUZ <maria_eg_cruz@estudante.sesisenai.org.br>
2023-09-21 08:12:53 -0300

    cadastro_revisar

--- web/src/screens/SignUp/index.tsx
+++ web/src/screens/SignUp/index.tsx
@@ -6,33 +6,33 @@ const RegisterForm = () => {
   return (
     <div className="register-container">
       <div className="div-logo">
-        <img src={LogoSingUp} className='Logo' />
+        <img src={LogoSingUp} className='Logo' alt="Logo" />
       </div>
       <div className="div-content">
         <h2>Cadastrar</h2>
         
         <div className="input-container">
-          <h3 className="input-label">E-mail</h3>
-          <input className='input-email' type="email" placeholder="seuemail@estudante.sesisenai.org.br" />
+          <label htmlFor="email" className="input-label">E-mail</label>
+          <input id="email" className='input-email' type="email" placeholder="seuemail@estudante.sesisenai.org.br" />
         </div>
         <div className="input-left">
           <div className="input-senha">
-            <h3 className="input-label">Senha</h3>
-            <input className='input-email' type="password" placeholder="senha" />
+            <label htmlFor="senha" className="input-label">Senha</label>
+            <input id="senha" className='input-email' type="password" placeholder="senha" />
           </div>
           <div className="input-senha">
-            <h3 className="input-label">Confirmar Senha</h3>
-            <input className='input-email' type="password" placeholder="senha" />
+            <label htmlFor="confirmar-senha" className="input-label">Confirmar Senha</label>
+            <input id="confirmar-senha" className='input-email' type="password" placeholder="senha" />
           </div>
         </div>
         <div className="input-right">
           <div className="input-telefone">
-            <h3 className="input-label">Telefone</h3>
-            <input className='input-email' type="number" placeholder="48 99999-9999" />
+            <label htmlFor="telefone" className="input-label">Telefone</label>
+            <input id="telefone" className='input-email' type="number" placeholder="48 99999-9999" />
           </div>
           <div className="input-turma">
-            <h3 className="input-label">Turma</h3>
-            <input className='input-email' type="text" placeholder="XX" />
+            <label htmlFor="turma" className="input-label">Turma</label>
+            <input id="turma" className='input-email' type="text" placeholder="XX" />
           </div>
         </div>
         <button className="submit-button">Cadastrar</button>
