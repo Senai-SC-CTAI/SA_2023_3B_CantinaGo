8332e37053292cf4d9cb19896c0095138515fc5c
ANA BEATRIZ MARTINS <ana_beatriz-martins@estudante.sesisenai.org.br>
2023-10-04 10:10:19 -0300

    FEEDBACK MOSTRANDO VNO WEEBBBBB!

--- back/src/main/java/com/example/cantinago/controller/FeedbackController.java
+++ back/src/main/java/com/example/cantinago/controller/FeedbackController.java
@@ -5,7 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import java.util.List;
 import org.springframework.http.ResponseEntity;
-@CrossOrigin(origins = "http://localhost:19006")//Endereço do front
+@CrossOrigin(origins = {"http://localhost:5173","http://localhost:19006"}) // Endereço do front app e web
 @RestController
 @RequestMapping("/feedback")
 
