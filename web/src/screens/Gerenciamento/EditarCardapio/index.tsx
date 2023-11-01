c751277c4e36c9d372c7a0b09b89685182f8f067
MARIA EDUARDA GARIBALDI DA CRUZ <maria_eg_cruz@estudante.sesisenai.org.br>
2023-11-01 11:44:52 -0300

    cardapio

--- web/src/screens/Gerenciamento/EditarCardapio/index.tsx
+++ web/src/screens/Gerenciamento/EditarCardapio/index.tsx
@@ -1,6 +1,7 @@
 import './style.css'
 import Header from '../../../components/Header/index'
-
+import {Link} from "react-router-dom"
+import { FeedbackComponent } from "../Feedback"
 
 function App() {
 
@@ -8,8 +9,33 @@ function App() {
     <>
       <Header />
 
-      <main>
+      <main className='mainGerenciamento'>
+        <section>
+          <article className='graphic1Gerenciamento'>
+
+          </article>
+          <div className='buttonsGerenciamento'>
+            <Link to='/ListaComidas'>
+              <p className='buttonGerenciamento'>Registrar cardapio</p>
+            </Link>
+            <Link to='/EditarCardapio'>
+              <p className='buttonGerenciamento'>Resetar Cardapio</p>
+            </Link>
+            <Link to='/EditarCardapio'>
+              <p className='buttonGerenciamento'>Cadastrar comida</p>
+            </Link>
+          <FeedbackComponent />
+          </div>
+        </section>
+
+        <section>
+          <article className='graphic2Gerenciamento'>
        
+          </article>
+          <article className='graphic3Gerenciamento'>
+
+          </article>
+        </section>
       </main>
     </>
   )
