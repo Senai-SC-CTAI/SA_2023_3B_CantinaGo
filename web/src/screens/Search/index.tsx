4658628007a2ed2b2c762a9cd0c40f072a9aedf3
Bigborg <100381400+EnzoBDF@users.noreply.github.com>
2023-11-09 11:59:41 -0300

    Update index.tsx

--- web/src/screens/Search/index.tsx
+++ web/src/screens/Search/index.tsx
@@ -1,49 +1,59 @@
-import './style.css'
-import Header from '../../components/Header/index'
-import Footer from '../../components/Footer/index'
-import Card from '../../components/Card'
-import axios from 'axios'
+import './style.css';
+import Header from '../../components/Header/index';
+import axios from 'axios';
 import React, { useState, useEffect, ChangeEvent } from 'react';
+import Card from '../../components/Card';
 
+interface Comida {
+  id: number;
+  nome: string;
+  foto: string;
+  preco: number;
+}
 
 function App() {
+  const [comida, setComida] = useState<string>('');
+  const [comidas, setComidas] = useState<Comida[]>([]);
 
-  const [comida, setComida] = useState<string>();
-  const [comidas, setComidas] = useState([]);
-
-
-/*   const mostrarResultados = {
-    Array.forEach(element => {
-      
-    });
-
-  } */
+  const renderComidas = () => {
+    return comidas.map((comida) => (
+      <div key={comida.id} className="comidaContainer">
+        <img src={comida.foto} alt={comida.nome} />
+        <div className="div">
+        <h4>{comida.nome}</h4>
+        <p>Preço: R${comida.preco.toFixed(2)}</p>
+        </div>
+      </div>
+    ));
+  };
 
   const fetchComidas = async () => {
     try {
       const response = await axios.get(`http://127.0.0.1:8090/comidas/${comida}`);
-      
       setComidas(response.data);
     } catch (error) {
       console.log("error", error);
     }
   };
 
-
-    const handleInputChange = (event: ChangeEvent<HTMLInputElement>) => {
-      const enteredName = event.target.value;
-      setComida(enteredName);
-      fetchComidas();
-    };
+  const handleInputChange = (event: ChangeEvent<HTMLInputElement>) => {
+    const enteredName = event.target.value;
+    setComida(enteredName);
+    fetchComidas();
+  };
 
   return (
-    <><section className='mainSearch'>
-      <Header/>
+    <section className='mainSearch'>
+      <Header />
       <div className="searchContainer">
         <h3>Buscar</h3>
-        <input type="text" value={comida} onChange={handleInputChange}/>
+        <input type="text" value={comida} onChange={handleInputChange} />
       </div>
-      <h1>Salgados</h1>
+     
+          <div className="pesquisaComida">
+            {renderComidas()}
+          </div>
+          <h1>Salgados</h1>
       <div className="typeContainer">
         <Card/>
         <Card/>
@@ -84,9 +94,15 @@ function App() {
         <Card/>
         <Card/>
       </div>
+      
+    
+      
+      {/* Renderizar dinamicamente as categorias */}
+  
     </section>
-    </>
-  )
+  );
 }
+
+export default App;
 
 export default App
