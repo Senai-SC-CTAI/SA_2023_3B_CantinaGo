88a74c303b67af7d3f30b1b6c74838e3fb9a241f
ANA BEATRIZ MARTINS <ana_beatriz-martins@estudante.sesisenai.org.br>
2023-11-09 12:04:23 -0300

    :ok_hand: statistics

---
+++ web/src/screens/Statics/index.tsx
@@ -0,0 +1,16 @@
+import Footer from "../../components/Footer";
+import Header from "../../components/Header";
+import './styles.css'
+
+export default function Statics() {
+    return (
+        <>
+            <Header />
+            <div className="containerStatics">
+                <h1 className="title">Estatísticas</h1>
+                
+            </div>
+            <Footer />
+        </>
+    )
+}
